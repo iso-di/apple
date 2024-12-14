5ef07e3ac0bc15f685fbf4df5e8b9448b98b9a21
iso-di <dsolodovn@icloud.com>
2024-12-14 03:05:44 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -384,7 +384,18 @@
 					});
 
 			}
-			
+			// Код pwa.
+
+			if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((registration) => {
+      console.log('Service Worker зарегистрирован:', registration);
+    })
+    .catch((error) => {
+      console.error('Ошибка регистрации Service Worker:', error);
+    });
+}
 
 		// Initialize.
 
